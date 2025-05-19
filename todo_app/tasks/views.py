649b2069669261b3a76c5ee0d68465603d1e192a
Goutami Sooda <soodagoutami@gmail.com>
2025-05-19 17:14:28 +0530

    Add templates code with basic html and javascript

--- todo_app/tasks/views.py
+++ todo_app/tasks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
+# def home(request):
+#     return HttpResponse("Hello, Django! Welcome to the To-Do App.")
+
 def home(request):
-    return HttpResponse("Hello, Django! Welcome to the To-Do App.")
+    return render(request, 'home.html')
